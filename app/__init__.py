26f660c49a47934b148e08db5e11f835c68030a6
aleksandrbarinov <barabarinov@gmail.com>
2022-01-25 18:24:13 +0200

    added requirements

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,5 @@
+import os
+
 from flask_api import FlaskAPI
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
@@ -7,7 +9,10 @@ app = FlaskAPI(__name__)
 app.secret_key = 'some secret key'
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/postgres'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv(
+    'DATABASE_URI',
+    'postgresql://postgres:postgres@localhost:5432/pet_shop',
+)
 db = SQLAlchemy(app)
 
 
@@ -25,6 +30,6 @@ def load_user(user_id):
 from app.routes import *
 from app.models import *
 
-import ipdb; ipdb.set_trace()
-db.drop_all()
-db.create_all()
+
+# db.drop_all()
+# db.create_all()
